1736f27ea240cae7f38ba3cfd4df587766164c6f
Sergey Belelya <sergey.belelya2014@gmail.com>
2016-05-15 21:18:43 +1000

    fix

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var connString = 'postgres://aokfxrxipljadk:H1sGlA1o88iWCfZFvaLZeeJ1Sk@ec2-23-21-238-76.compute-1.amazonaws.com:5432/d781h39ucu0rs2'
+var connString = 'postgres://aokfxrxipljadk:H1sGlA1o88iWCfZFvaLZeeJ1Sk@ec2-23-21-238-76.compute-1.amazonaws.com:5432/d781h39ucu0rs2';
 
 var pg = require('pg');
 var express = require('express');
@@ -8,14 +8,14 @@ var port = process.env.PORT || 5000;
 app.get('/', function (req, res) {
     res.sendfile('index.html');
 
-/*    pg.connect(connString, function (err, client, done) {
-        if (err) res.send("Could not connect to DB: " + err);
-        client.query('SELECT * FROM MyTable', function (err, result) {
-            done();
-            if (err) return res.send(err);
-            res.send(result.rows);
-        });
-    });*/
+    /*    pg.connect(connString, function (err, client, done) {
+     if (err) res.send("Could not connect to DB: " + err);
+     client.query('SELECT * FROM MyTable', function (err, result) {
+     done();
+     if (err) return res.send(err);
+     res.send(result.rows);
+     });
+     });*/
 });
 
 app.get('/auth', function (req, res) {
@@ -26,16 +26,16 @@ app.get('/auth', function (req, res) {
     //res.end(login + ' ' + pass);
 
     pg.connect(connString, function (err, client, done) {
-        if (err)
-            res.send("Could not connect to DB: " + err);
+        if (err) res.send("Could not connect to DB: " + err);
         client.query('SELECT * FROM MyTable', function (err, result) {
             done();
-            if (err)
-                return res.send(err);
+            if (err) return res.send(err);
+            console.log(result.rows);
             res.send(result.rows);
-
         });
     });
+
+    res.end("ok");
 });
 
 
